79f5ff557bc21dee06ce7b1cc79a9e1aaf6c2e23
Kawser Ahmed Roni <kawserahmedroni51@gamil.com>
2024-09-04 01:07:46 +0600

    tf_startUp commited

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+/*-----------------------------------------------------------------
+
+Template Name:  Edutek- Education & Online Courses Html Template
+Author:  Gramentheme
+Author URI: https://themeforest.net/user/gramentheme/portfolio
+Developer: Kawser Ahmed Roni
+Version: 1.0.0
+Description: Edutek- Education & Online Courses Html5 Template
+
+-------------------------------------------------------------------
+CSS TABLE OF CONTENTS
+-------------------------------------------------------------------
+
+01. preloader
+02. header
+03. swiper slider
+04. animated text with swiper slider
+05. shop products count
+06. image src change
+07. hide & show a div
+08. isotope
+09. add class & remove class
+10. magnificPopup
+11. back to top
+12. data backgrund
+13. coundown by click
+14. remove products
+15. wow animation
+
+------------------------------------------------------------------*/
+
+(function ($) {
+	"use strict";
+	// Preloader area start here ***
+
+	// Preloader area end here ***
+})(jQuery);
